a9fa0336456c4bcff9140a90fb6d4342962198a8
owennyakagwa <97963120+owennyakagwa@users.noreply.github.com>
2022-03-01 23:09:33 +0300

    Update main.c

--- main.c
+++ main.c
@@ -22,8 +22,43 @@ int menu() {
 }
 int main()
 {
-    printf("\t County Library System\n");
-    printf("Welcome Mr. Owen!\n");
-    printf("You selected action %d",menu());
-    return 0;
+int menu() {
+    int action;
+   printf("Select an action below\n");
+   printf("1. Add a new patron\n");
+   printf("2. View all patron\n");
+   printf("3. View all books\n");
+   printf("4. Add new book\n");
+   printf("Your action:");
+   scanf("%d",&action);
+   if(action < 1 || action > 4) {
+    printf("invalid action. Try again\n");
+   }
+   return action;
 }
+
+void execute_action(int action) {
+    switch(action) {
+    case 1:
+        printf("1. Add a new patron\n");
+        break;
+    case 2:
+        printf("List of all patrons\n");
+        break;
+    case 3:
+        printf("List of all books\n");
+        break;
+    case 4:
+        printf("Adding a new book\n");
+        break;
+    default:printf("invalid action.\n");
+    }
+}
+
+int main()
+{
+    printf("\t County Library System\n");
+    printf("Welcome Mr owen!\n");
+    execute_action(menu());
+    return 0;
+
